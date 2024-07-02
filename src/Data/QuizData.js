178a1323e224c5d3c01186131dc24156c258ebac
PRERNA-1825 <101181225+PRERNA-1825@users.noreply.github.com>
2024-07-02 18:34:19 +0530

    Add files via upload

---
+++ src/Data/QuizData.js
@@ -0,0 +1,23 @@
+export const QuizData = [
+    {
+      question: "What is the capital of Japan?",
+      options: ["Beijing", "Seoul", "Tokyo", "Bangkok"],
+      answer: 3
+    },
+    {
+      question: "Who wrote 'To Kill a Mockingbird'?",
+      options: ["Harper Lee", "Mark Twain", "J.K. Rowling", "Ernest Hemingway"],
+      answer: 1
+    },
+    {
+      question: "What is the largest planet in our solar system?",
+      options: ["Earth", "Mars", "Jupiter", "Saturn"],
+      answer: 3
+    },
+    {
+      question: "In what year did the Titanic sink?",
+      options: ["1912", "1905", "1918", "1923"],
+      answer: 1
+    },
+  ];
+  
